569d350dcba2cff8a3bbdc38e278b2c5d3435f84
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-12 21:39:47 -0500

    Complete Backend and Frontent of OPPConcepts

---
+++ OPPConcepts/OppConcepts.Frontend/Program.cs
@@ -0,0 +1,18 @@
+﻿using OppConcepts.Backend;
+
+    try
+{
+    var date1 = new Date();
+    var date2 = new Date(2028, 2, 29);
+    var date3 = new Date(2012, 11, 21);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);  
+}

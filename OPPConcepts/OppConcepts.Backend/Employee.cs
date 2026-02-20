399436b48302b8a1675423507de268adce761aa2
BRAYAN ESTEBAN MONTOYA LONDO�O <brayanmontoya1126335@correo.itm.edu.co>
2026-02-19 23:41:01 -0500

    Complete class Employe

---
+++ OPPConcepts/OppConcepts.Backend/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace OppConcepts.Backend
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public string LastName { get; set; }
+
+        public Date BornDate { get; set; }
+
+        public Date HireDate { get; set; }
+        
+        public Employee() { 
+        }
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id} {FirstName} ({LastName})";
+        }
+
+    }
+}
+
+    
+
